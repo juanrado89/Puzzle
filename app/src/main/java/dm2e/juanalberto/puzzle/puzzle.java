b42d87a6217023197e2609ca9c6e6813b67cbaed
juanrado89 <juan.rado@educa.madrid.org>
2024-01-19 13:58:52 +0100

    se termina de crear el manejo del puzzle, falta la base de datos y el segundo puzzle

--- app/src/main/java/dm2e/juanalberto/puzzle/puzzle.java
+++ app/src/main/java/dm2e/juanalberto/puzzle/puzzle.java
@@ -1,14 +1,24 @@
 package dm2e.juanalberto.puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -16,49 +26,120 @@ public class puzzle extends AppCompatActivity {
 
     TextView nom;
     TextView result;
-    ImageView imagen1;
-    ImageView imagen2;
-    ImageView imagen3;
-    ImageView imagen4;
-    ImageView imagen5;
-    ImageView imagen6;
-    ImageView imagen7;
-    ImageView imagen8;
+    GridLayout gridLayout;
+    int contador = 0;
+
+    private ImageView imagenSeleccionada1;
+    private ImageView imagenSeleccionada2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
+
         nom = findViewById(R.id.textoNombre);
+        result = findViewById(R.id.puntuacion);
         Intent inicio = getIntent();
         String nombre = inicio.getStringExtra("nombre");
         nom.setText(getString(R.string.ordena_el_puzzle) + nombre);
-        ImageView[] imagenes = {
-                imagen1 = findViewById(R.id.imagen1),
-        imagen2 = findViewById(R.id.imagen2),
-        imagen3 = findViewById(R.id.imagen3),
-        imagen4 = findViewById(R.id.imagen4),
-        imagen5 = findViewById(R.id.imagen5),
-        imagen6 = findViewById(R.id.imagen6),
-        imagen7 = findViewById(R.id.imagen7),
-        imagen8 = findViewById(R.id.imagen8)
-        };
-        List<Integer> listaNumeros = new ArrayList<>();
-        Random rd = new Random();
-        int numero = 0;
-        for(ImageView imagen:imagenes){
-            do{
-                numero = rd.nextInt(8);
-                if(!listaNumeros.contains(numero)){
-                    listaNumeros.add(numero);
-                    switch (numero){
-                        case 1:
-                            Drawable imagen = Drawable.createFromPath("@drawable/imagen1");
-                            imagen.setImageDrawable(imagen);
-                    }
-                }
-            }while(!listaNumeros.contains(numero));
 
+        gridLayout = findViewById(R.id.imagenes);
+        ImageView[] imagenes = {
+                findViewById(R.id.imagen1),
+                findViewById(R.id.imagen2),
+                findViewById(R.id.imagen3),
+                findViewById(R.id.imagen4),
+                findViewById(R.id.imagen5),
+                findViewById(R.id.imagen6),
+                findViewById(R.id.imagen7),
+                findViewById(R.id.imagen8)
+        };
+
+        Integer[] fotos = {
+                R.drawable.imagen1,
+                R.drawable.imagen2,
+                R.drawable.imagen3,
+                R.drawable.imagen4,
+                R.drawable.imagen5,
+                R.drawable.imagen6,
+                R.drawable.imagen7,
+                R.drawable.imagen8
+        };
+        List<Integer> listaImagenes = Arrays.asList(fotos);
+        for(int i = 0; i < imagenes.length;i++){
+            Drawable drawable = ResourcesCompat.getDrawable(getResources(), listaImagenes.get(i), null);
+            imagenes[i].setTag(drawable);
         }
 
+        Collections.shuffle(listaImagenes);
+
+        for (int i = 0; i < listaImagenes.size(); i++) {
+            Drawable drawable = ResourcesCompat.getDrawable(getResources(), listaImagenes.get(i), null);
+            imagenes[i].setImageDrawable(drawable);
+            imagenes[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onImageClick((ImageView) view);
+
+                }
+            });
+        }
+
+        result.setText(getString(R.string.numero_de_intentos) + contador);
+
+
+    }
+
+    private void onImageClick(ImageView imageView) {
+
+        if (imagenSeleccionada1 == null) {
+            imagenSeleccionada1 = imageView;
+            imageView.setBackgroundColor(Color.BLUE);
+        } else {
+            imagenSeleccionada2 = imageView;
+            intercambiarImagenes(imagenSeleccionada1, imagenSeleccionada2);
+            contador++;
+            result.setText(getString(R.string.numero_de_intentos) + contador);
+            imagenSeleccionada1.setBackgroundColor(Color.TRANSPARENT);
+            imagenSeleccionada1 = null;
+            imagenSeleccionada2 = null;
+            if(verificarCoincidenciaDrawables()){
+                Toast mensaje = Toast.makeText(getApplicationContext(),"Has ganado!!!", Toast.LENGTH_LONG);
+                mensaje.show();
+            }
+        }
+    }
+
+    private void intercambiarImagenes(ImageView imagen1, ImageView imagen2) {
+        Drawable tempDrawable = imagen1.getDrawable();
+        imagen1.setImageDrawable(imagen2.getDrawable());
+        imagen2.setImageDrawable(tempDrawable);
+    }
+
+    private boolean verificarCoincidenciaDrawables() {
+        ImageView[] imagenes = {
+                findViewById(R.id.imagen1),
+                findViewById(R.id.imagen2),
+                findViewById(R.id.imagen3),
+                findViewById(R.id.imagen4),
+                findViewById(R.id.imagen5),
+                findViewById(R.id.imagen6),
+                findViewById(R.id.imagen7),
+                findViewById(R.id.imagen8)
+        };
+
+        for (ImageView imageView : imagenes) {
+            Drawable drawable = imageView.getDrawable();
+            Drawable comprobacion = (Drawable) imageView.getTag();
+
+            if (drawable != null && comprobacion != null) {
+
+                if (!drawable.getConstantState().equals(comprobacion.getConstantState())) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
